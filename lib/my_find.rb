b2dfe65d9f32c242bc4db3c10196e35c8c674413
Aiden Marques <aiden.marques94@gmail.com>
2020-09-20 04:45:30 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,12 @@ def my_find(collection)
   i = 0 
   
   while i < collection.length 
-    yield (collection[i])
+    if yield (collection[i])
+      return collection[i]
+    end
+    
     i += 1 
   end
   
-  if my_find == true 
+  
 end
